cfc1eb014824d0ad6a3e76364aac4ec54ff39c53
Penny <tienpei2014@gmail.com>
2016-06-07 03:47:51 +0800

    new idea

--- OSp4.cpp
+++ OSp4.cpp
@@ -6,97 +6,99 @@
 #include <pthread.h> 
 #include <mutex>
 #include <vector>
+#include <queue>
 using namespace std;
  
 mutex mutex1;
 mutex mutex2;
 
-struct TimeLine
+/*struct TimeLine
 {
 	int time;
 	vector<int> waiting;
 	int tutring;
-};
+};*/
 
 struct Person
 {
+	int questionTime;
 	int ID;
-	int schedual[100000];
 };
 
 struct Data {
-	int now;
-	TimeLine timeline[100000];
+	//TimeLine timeline[10000];
+	int tutoring;
+	queue<int> waiting;
 	Person person[31];
    	int numOfTutor;
    	int count[31];
 };
 
+int now;
 int state = 0;
+int count = 0;
+int term = 0;
  
 void* Threading(void* ptr) {
 	Data *data = (Data *)ptr;
 
-	mutex.lock();
-	int *now = &data->now;
-	TimeLine *timeline = data->timeline;
-
+	mutex1.lock();
 	int id = data->person[state].ID;		//told to every thread it's responce person
 	Person *me = &data->person[id];
 
 	state++;
-	mu.unlock();
+	mutex1.unlock();
 
 	while(state != 31)					//wait until every thread been told
 		;
 
 	while(1)
 	{
-		mu.lock();
-		if (data->numOfTutor>=0)
+		mutex1.lock();
+		if (term < 31)
 		{
-			//stdent routine section
-			int randomtime = rand()%31 + *now;	 	//	random asking time
-			// check the personal schedual first
+			if (data->numOfTutor>=0)
+			{
+				//stdent routine section
 
-			if (data->timeline[data->now].tutring == 0)	//if ta is naping
-			{
-				//just get in.
-				//tutor is your id
-				//update personal schedual
-				//remember its 3 second
+				if (me->questionTime == 0)
+					me->questionTime = rand()%31 + now;	 	//	random asking time
+				//cout<<id<<" questionTime is "<<me->questionTime<<endl;
+				//data->count[id]++;
+				if (me->questionTime == now)
+				{
+					if (data->tutoring == 0)
+						data->tutoring = id;
+					else if (data->waiting.size()<3)
+						data->waiting.push(id);
+					else
+						me->questionTime += 5;
+				}
+				// check the personal schedual first
+				data->numOfTutor--;
 			}
-			else if (data->timeline[data->now].waiting.size()<3)// the waiting line
+			else
 			{
-				//update personal scheual, timline, 
-				//
-			}
-			else if (data->timeline[data->now].waiting.size()>=3)
-			{
-				//personal schedual from now to now+5 is -1
+				mutex1.unlock();
+				break;
 			}
 		}
-		else
-		{
-			mu.unlock();
-			break;
-		}
-		mu.unlock();
+
+		mutex1.unlock();
    	}
-   mutex.lock();
 
-
+   	mutex1.lock();
    cout<<id<<" : "<<data->count[id]<<endl;			//count how many time the thread be in lock
-   mu.unlock();
+   mutex1.unlock();
    return NULL;
 }
 
 void* teachingAssistant(void* ptr) 
 {
 	mutex2.lock();
-	while(sum!=15)
-		;
-	if  (sum == 15)
+	//while(sum!=15)
+	//	;
+	//if  (sum == 15)
 	{
 		cout<<"middle~~"<<endl;
 		mutex2.unlock();
@@ -109,27 +111,23 @@ int main( ) {
 
 	Data data;
 	
-	
 	pthread_t *thread1 = new pthread_t[31] ;
 
 	for (int i = 0; i < 31; ++i)
 	{
 		data.person[i].ID = i;
+		data.person[i].questionTime = 0;
 		data.count[i] = 0;	
 	}
-	data.numOfTutor = 100;
-	data.now = 0;
 
-	pthread_create (&thread1[0], NULL, teachingAssistant, &data); 
-	for (int i = 1; i < 31; )
+	data.numOfTutor = 30;
+
+	for(int i = 0; i < 31; )
 		 pthread_create (&thread1[i++], NULL, Threading, &data); 
 	
-
-
-	for (int i = 0; i < 30; ++i)
-	{
+	for (int i = 0; i < 31; ++i)
 		pthread_join(thread1[i], NULL);
-	}
+	
 
     return 0;
 }
