9d2711910502aeb4595d3d64a762e096e4209bac
Penny <tienpei2014@gmail.com>
2016-06-08 05:05:04 +0800

    check already.

--- OSp4.cpp
+++ OSp4.cpp
@@ -13,19 +13,12 @@ mutex mutex1;
 mutex mutex2;
 
 /*剩下
-1.waiting 只有一入隊伍時cout，目前每秒 cout 一次
-	想改將 vector<int> waiting 改ㄓㄥ vector<Waiting> waiting
-	waiting 裡有 int id 以及 int arriveTime
-2.助教上廁所
-3.助教兩位
-4.排版*/
+1.助教上廁所
+2.助教兩位
+*/
 
-/*struct TimeLine
-{
-	int time;
-	vector<int> waiting;
-	int tutring;
-};*/
+//我假設 學生出來與下一位學生來為同時間 所以椅子上依然為3人 所以無法排！
+
 
 struct Student
 {
@@ -50,10 +43,12 @@ struct Data {
 int now;
 int state = 0;
 int count = 0;
-int term = 0;
 int temp = 2;
 
 void printTime(int second);
+void printIdentity(int id);
+void tutor(int id);
+void leave(int id);
  
 void* Threading(void* ptr) {
 	Data *data = (Data *)ptr;
@@ -71,79 +66,101 @@ void* Threading(void* ptr) {
 	while(1)
 	{
 		mutex1.lock();
-		if (data->numOfTutor<=60)
+		if (data->numOfTutor<=60)//！！這個放這邊 ta 收尾麻煩！
 		{
-			if ( id != 30)										//Student
+			if ( id != 30 )										//Student
 			{
 				//stdent routine section
 
+				//the time this student go to ask question
 				if (me->questionTime == 0 && me->questionFrequency == 0)
 					me->questionTime = rand()%31 + now;	 	//	first asking
 				else if (me->questionTime == 0 && me->questionFrequency != 0)
-					me->questionTime = (rand()%21+10) + now; //rand()%21+10 = 10 - 30 sec
+					me->questionTime = (rand()%21+10) + now; 	//not the first time asking.
 
 				//cout<<id<<" questionTime is "<<me->questionTime<<endl;
 				data->count[id]++;
+
 				if (me->questionTime == now)
 				{
-					if (data->tutoring == 0)
+					if (data->tutoring == -1)//now don't have any one asking
 					{
 						data->tutoring = id;
-						me->questionTime = rand()%30 + now + 3;
+						//me->questionTime = 0;
+						//！突然想到，questionTime 的初始化得在問完問題再做！
+						//me->questionFrequency++;
 					}
 					else if (data->waiting.size()<3)
 					{
-						WaitingLine temp;
+						WaitingLine temp;				//add to waiting line
 						temp.ID = id;
 						temp.arriveTime = now;
 						data->waiting.push_back(temp);
-
-						me->questionTime = rand()%30 + 3*(data->waiting.size()+1) + now;
+						//me->questionTime = rand()%30 + 3*(data->waiting.size()+1) + now;
+						//！突然想到，questionTime 的初始化得在問完問題再做！
+						//現在就讓它停在 now 反正助教一定在這後面，下一次跑到這個 thead 一定已經跑過這一輪助教
 					}
-					else
+					else						//come back after 5 second.
 						me->questionTime += 5;
-
 				}
-				// check the Studental schedual first
-				//data->numOfTutor--;
+
+
 			}
 			else													//TA
 			{
-				//cout<<"______________________________"<<endl;
-				//cout<<now<<": "<<endl;
-				if (data->tutoring == 0)
+				if (data->tutoring == -1)
 				{
 					printTime(now);
 					cout<<"-TA: Napping"<<endl;
+					//這裏做廁所時間 rand ％2來決定是否廁所
+					//好像得做一個開關先在助教沒空得排隊也
 				}
 				else
 				{
-					if (temp == 2)
+					if (temp == 2)//The first second the student tutoring.
 					{
-						printTime(now);
-						cout<<"-Student"<<data->tutoring<<": Asking--"<<data->numOfTutor<<endl;
-						data->student[data->tutoring].questionFrequency++;
+						//跟下面重複想辦法合併
+						tutor(data->tutoring);
+						/*printTime(now);
+						cout<<"-Student";
+						printIdentity(data->tutoring);
+						cout<<": Asking--"<<data->numOfTutor<<endl;*/
+
 						data->numOfTutor++;
 						temp--;
 					}
-					else if (temp > 0)
+					else if (temp > 0)//The second second the student tutoring.
 						temp--;
-					else
+					else				//The thirsd second the student tutoring.
 					{
-						printTime(now);
-						cout<<"-Student"<<data->tutoring<<" Exit to programming"<<endl;
+						/*printTime(now);
+						cout<<"-Student";
+						printIdentity(data->tutoring);
+						cout<<": Exit to programming"<<endl;*/
+						leave(id);
+
+						data->student[data->tutoring].questionFrequency++;
+						//初始化
+						data->student[data->tutoring].questionTime = 0;
 						temp = 2;
 						if (data->waiting.size()>0)
 						{
 							data->tutoring = data->waiting.front().ID;
 							vector<WaitingLine>::iterator it = data->waiting.begin();
 							data->waiting.erase(it);
-							printTime(now);
-							cout<<"-Student"<<data->tutoring<<": Asking--"<<data->numOfTutor<<endl;
-							data->student[data->tutoring].questionFrequency++;
+
+							tutor(data->tutoring);
+							/*printTime(now);
+							cout<<"-Student";
+							printIdentity(data->tutoring);
+							cout<<": Asking--"<<data->numOfTutor<<endl;*/
+
+
 							data->numOfTutor++;
 							temp--;
 						}
+						else
+							data->tutoring = -1;
 					}
 
 
@@ -152,10 +169,13 @@ void* Threading(void* ptr) {
 							if (data->waiting[i].arriveTime == now)
 							{
 								printTime(now);
-								cout<<"-Student"<<data->waiting[i].ID<<" Sitting #"<<i+1<<endl;
+								cout<<"-Student";
+								printIdentity(data->waiting[i].ID);
+								cout<<": Sitting #"<<i+1<<endl;
 							}
-					term = 0;
 				}
+				//int m = 0;
+				//cin>>m;
 				sleep(1);
 				now++;
 			}
@@ -163,34 +183,31 @@ void* Threading(void* ptr) {
 		}
 		else
 		{
+			if (id == 30)
+				{
+						/*printTime(now);
+						cout<<"-Student";
+						printIdentity(data->tutoring);
+						cout<<": Exit to programming"<<endl;*/
+						leave(id);
+						data->student[data->tutoring].questionFrequency++;
+						printTime(now);
+						cout<<"-TA: Exist."<<endl;
+				}
 			mutex1.unlock();
 			break;
 		}
 		mutex1.unlock();
    	}
 
-   	//mutex1.lock();
-   //cout<<id<<" : "<<data->count[id]<<endl;			//count how many time the thread be in lock
-   //mutex1.unlock();
+   /*mutex1.lock();
+   cout<<id<<" : "<<data->count[id]<<", ask: "<<me->questionFrequency<<" times."<<endl;			//count how many time the thread be in lock
+   mutex1.unlock();*/
    return NULL;
 }
-
-void* teachingAssistant(void* ptr) 
-{
-	mutex2.lock();
-	//while(sum!=15)
-	//	;
-	//if  (sum == 15)
-	{
-		cout<<"middle~~"<<endl;
-		mutex2.unlock();
-	}
-	
-    return NULL;
-}
  
-int main( ) {
-
+int main( )
+{
 	Data data;
 	
 	pthread_t *thread1 = new pthread_t[31] ;
@@ -204,6 +221,7 @@ int main( ) {
 	}
 
 	data.numOfTutor = 0;
+	data.tutoring = -1;
 
 	for(int i = 0; i < 31; )
 		 pthread_create (&thread1[i++], NULL, Threading, &data); 
@@ -234,6 +252,29 @@ void printTime(int now)
 	else
 		cout<<second;
 
+}
+void printIdentity(int id)
+{
+	if (id<10)
+		cout<<0<<id;
+	else
+		cout<<id;
+}
+
+void tutor(int id)
+{
+	printTime(now);
+	cout<<"-Student";
+	printIdentity(id);
+	cout<<": Asking"<<endl;//<<data->numOfTutor<<endl;
+}
+
+void leave(int id)
+{
+	printTime(now);
+	cout<<"-Student";
+	printIdentity(id);
+	cout<<": Exit to programming"<<endl;
 }
 
 
